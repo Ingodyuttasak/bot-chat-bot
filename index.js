af7ffebec917a2ff85d3e300d152c24d974e8693
RVCONNEX\Yuttasak.R <in117dk54@gmail.com>
2021-10-20 10:02:35 +0700

    commits

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ app.post('/callback', line.middleware(config), (req, res) => {
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    console.log(`user ID: ${event.source.userId}`);
+    
     // ignore non-text-message event
     return Promise.resolve(null);
     
   }
-  
+  console.log(`user ID: ${event.source.userId}`);
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
 
