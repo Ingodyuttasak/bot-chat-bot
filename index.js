e4843cad0328edfb7eeefab5e8bfd1c16861afcb
RVCONNEX\Yuttasak.R <in117dk54@gmail.com>
2021-10-21 13:39:42 +0700

    see names

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
-'use strict';
-
+require('dotenv').config();
 const line = require('@line/bot-sdk');
 const express = require('express');
+const bodyParser = require('body-parser');
+const { request } = require('express');
 
-// create LINE SDK config from env variables
+
 const config = {
-  channelAccessToken: 'yVkhxm9IviVHL0E+lIh/G+3wQy2E1pD8SAbDrDCjFQrZsHM4z6EnD3bNqKoxHvUZVPtwlCkmqAWAIua1HyZSdj1k/Xpozurpdw+jMHN3aW7+p31NuEkixpkzhCeWjxpvwAiTajmVUsYU+oCcN4mWMwdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'af0fbb8cde9ef94f4debb50a08efa3e1',
+  channelAccessToken:process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// create LINE SDK client
-const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
+const client = new line.Client(config);
 const app = express();
 
-app.get('/',(req,res)=>{
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/testendpoint',(req,res)=>{
   return res.status(200).json({
-    status: 'sccess',
-    message: 'Connected',
+    status: 'success',
+    message :'Connected',
   })
 })
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    
-    
-    // ignore non-text-message event
-    return Promise.resolve(null);
-    
+app.post('/webhook',(req,res)=>{
+  console.log('webhook request');
+  console.log(req.body);
+  
+  replyToken = req.body.events[0].replyToken;
+  msg = req.body.events[0];
+  reply(replyToken,msg);
+  res.send('Nice!!!')
+})
+
+//function ที่สร้างขึ้นเพื่อตอบกลับ user 
+function reply(replyToken,msg){
+  let headers = {
+    'Content-type': 'application/json',
+    'Authorization' : 'Bearer : process.env.CHANNEL_ACCESS_TOKEN '//channal access token
   }
-  console.log(` ID: ${event}`);
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  let body = JSON.stringify({
+    replyToken: replyToken,
+    messages : 0[
+      {
+      type: 'text',
+      text :'Hello from webhook test',
+      },
+      {
+      type: 'text',
+      text : msg
+      }
+  ]
+  })
+  request.post({
+    url: 'https://api.line.me/v2/bot/message/reply',
+    headers : headers,
+    body: body 
+  },
+  (err,res,body)=>{
+    console.log(err);
+    console.log('status = ' + res.statusCode);
+  }
+  )
 }
 
 
 
 
-// listen on port
+//Server Port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
